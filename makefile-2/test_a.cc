85b6bed5b06072fb6b100ad6b2330d2161742e51
cpppy <1107706059@qq.com>
2016-08-03 09:19:49 +0800

    Add files via upload

---
+++ makefile-2/test_a.cc
@@ -0,0 +1,7 @@
+#include "so_test.h"
+using namespace std; 
+void test_a()
+{
+	cout<<"this is in test_a..."<<endl;
+}
+
---
+++ makefile-2/test_b.cc
@@ -0,0 +1,12 @@
+#include "so_test.h"
+using namespace std;
+void test_b()
+{
+	cout<<"this is in test_b..."<<endl;
+}
+
+void test234()
+{
+	cout<<"function-234 defined in test_b"<<endl;
+}
+
