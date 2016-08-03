197774384fe0995c2379eebcaa68aac7a2d2ce06
cpppy <1107706059@qq.com>
2016-08-03 09:37:30 +0800

    Add files via upload

---
+++ makefile-2.1-like-a-project/src/so_test.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+void test_a();
+void test_b();
+void test_c();
+
+void test234();
+
---
+++ makefile-2.1-like-a-project/src/test_c.cc
@@ -0,0 +1,6 @@
+#include "so_test.h"
+using namespace std;
+void test_c()
+{
+	cout<<"this is in test_c..."<<endl;
+}
